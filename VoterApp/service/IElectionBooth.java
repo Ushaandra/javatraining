284edfebdb0b8fe3ae40712e1bc960dc93bad1f8
Ushaandra <107882176+Ushaandra@users.noreply.github.com>
2023-08-03 21:29:40 +0530

    Delete VoterApp directory

--- VoterApp/service/IElectionBooth.java
+++
@@ -1,7 +0,0 @@
-package com.voterapp.service;
-
-import com.voterapp.exception.InvalidVoterException;
-
-public interface IElectionBooth {
-	boolean checkEligibility(int age,String locality,int voterId) throws InvalidVoterException;
-}
