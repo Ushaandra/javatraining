231d021034786febf132414321c21187f093b8d9
Ushaandra <107882176+Ushaandra@users.noreply.github.com>
2023-08-09 23:23:52 +0530

    Add files via upload

---
+++ VoterApp/src/com/voterapp/exception/InvalidVoterException.java
@@ -0,0 +1,17 @@
+package com.voterapp.exception;
+
+public class InvalidVoterException extends Exception {
+
+	public InvalidVoterException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidVoterException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
